c481fa497a78f4b5ff0be311f3e810377f6127d8
Denis Bilash <faaakiring@gmail.com>
2021-04-02 12:12:20 +0300

    Added:  - User model Registered:  - User model in the admin panel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+from phone_field import PhoneField
+
+
+class User(AbstractUser):
+    is_leader = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+
+    phone = PhoneField(blank=False, help_text='Contact phone number')
