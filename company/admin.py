1e5c42c58afa5c6b6686ed6a27cce6a54901cca4
Denis Bilash <faaakiring@gmail.com>
2021-04-02 12:03:00 +0300

    Added:  - Company model;  - new dependency to requirements.txt Registered:  - Company model in the admin panel

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from company.models import Company
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
--- company/models.py
+++ company/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phone_field import PhoneField
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    phone = PhoneField(blank=True, help_text='Contact phone number')
+    email = models.EmailField(blank=True, help_text='Contact email')
