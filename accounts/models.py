652df0c1f54d6316f5856d8f0d8ba7ee4a17abd9
Denis Bilash <faaakiring@gmail.com>
2021-04-03 11:38:29 +0300

    Changed User model:  - PhoneField to CharField in the phone field

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
-from phone_field import PhoneField
-
-
 class User(AbstractUser):
     is_leader = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
 
-    phone = PhoneField(blank=False, help_text='Contact phone number')
+    phone = models.CharField(max_length=31, blank=False, help_text='Contact phone number')
