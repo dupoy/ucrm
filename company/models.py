12d8dd825cc7d64d004caeb4333f6221436f2298
Denis Bilash <faaakiring@gmail.com>
2021-04-03 13:54:58 +0300

    Changed Company model:  - PhoneField to CharField in the phone field

--- company/models.py
+++ company/models.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from phone_field import PhoneField
 
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     address = models.CharField(max_length=255)
-    phone = PhoneField(blank=True, help_text='Contact phone number')
+    phone = models.CharField(max_length=31, blank=True, help_text='Contact phone number')
     email = models.EmailField(blank=True, help_text='Contact email')
