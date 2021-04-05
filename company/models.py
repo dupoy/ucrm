2c8e72ceb57ef8419dc31420eed74f82e5e87410
Denis Bilash <faaakiring@gmail.com>
2021-04-05 18:13:52 +0300

    company crud fix

--- company/models.py
+++ company/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_delete, pre_delete
+from django.dispatch import receiver
 from django.urls import reverse_lazy
 
 User = get_user_model()
@@ -25,8 +27,16 @@ class Company(models.Model):
 
 
 class Manager(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='manager')
     company = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='managers')
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(pre_delete, sender=Company)
+def delete_user(sender, instance, **kwargs):
+    managers = Manager.objects.filter(company=instance)
+    for manager in managers:
+        user = User.objects.filter(manager=manager).first()
+        user.delete()
